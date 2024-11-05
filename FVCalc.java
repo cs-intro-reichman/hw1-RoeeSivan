a49eabee77dc082d39458edfb4228e4da8e2a915
Roee Sivan <roee.sivan@post.runi.ac.il>
2024-11-05 22:29:18 +0200

    task 2 another try

--- FVCalc.java
+++ FVCalc.java
@@ -2,12 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int CurrentVal = Integer.parseInt(args[0]);
-		int InterestRate = Integer.parseInt(args[1]);
-		Double Rate = (double)InterestRate/100;
+		double InterestRate = Double.parseDouble(args[1]);
+		double Rate = InterestRate/100;
 		//Double InterestRate = Double.parseDouble(args[1])
 		int n = Integer.parseInt(args[2]);  //number of years
-		Double power = Math.pow(1+Rate,n);
-		Double Futureval = (int)CurrentVal*power; 
-		System.out.println("After "+n +" years, a $"+CurrentVal + " saved at "+Rate*100 +"% will yield $" +Math.round(Futureval));
+		double power = Math.pow(1+Rate,n);
+		double Futureval = CurrentVal*power; 
+		System.out.println("After "+n +" years, a $"+CurrentVal + " saved at "+Rate*100 +"% will yield $" + (int) Futureval );
 	}
 }
