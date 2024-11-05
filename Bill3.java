6d27cc1958a12730f94a66e012d1cadb13c773b8
Roee Sivan <roee.sivan@post.runi.ac.il>
2024-11-05 18:01:38 +0200

    completed task 1 out of 5

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		double num = Double.parseDouble(args[3]);
+		Double amountPerPerson = Math.ceil(num/3);
+		System.out.println("Dear "+name1+","+" "+name2+", "+name3+","+" each of you needs to pay "+ amountPerPerson);
 	    // Replace this comment with the rest of your code   
 	}
 }
