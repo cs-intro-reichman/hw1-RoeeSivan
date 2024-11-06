9b7e3c805e54a679c62d0efe403ed8886d63e221
Roee Sivan <roee.sivan@post.runi.ac.il>
2024-11-06 15:55:53 +0200

    task 4 out of 5

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,18 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a= ((int)(Math.random()*lim));
+		int b=((int)(Math.random()*lim));
+		int c= ((int)(Math.random()*lim));
+		int sum = a+b+c;
+		System.out.println(a+" "+b+" "+c);
+		int MinNum = Math.min(a,b);
+		MinNum = Math.min (MinNum,c);
+		int MaxNum = Math.max(a,b);
+		MaxNum = Math.max (MaxNum,c);
+		int MiddleNum = sum -(MinNum+MaxNum);
+		System.out.println(MinNum+" "+MiddleNum+" "+MaxNum);
+
 	}
 }
